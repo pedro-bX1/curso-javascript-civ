9c31dbc9a51adf4ff6e7dfe085833787e08b6a3f
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2022-05-24 18:01:46 -0300

    DOM

---
+++ 1/ex07.js
@@ -0,0 +1,9 @@
+var vel = 75
+
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade, MULTADO!')
+}else{
+    console.log('Dirija sempre com segurança!')
+}
+
---
+++ 1/ex08.js
@@ -0,0 +1,17 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Brasileiro!')
+}else{
+    console.log('Estrangeiro')
+}
+
+var pais2 = 'Brasil'
+
+console.log(`Vivendo em ${pais2}`)
+if (pais2 != 'Brasil'){
+    console.log('Estrangeiro!')
+}else{
+    console.log('Brasileiro!')
+}
